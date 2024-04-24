c88efb73c1f6fd84cf685081e0bb20b211101eb9
smartchaoqi <qcq11111111@163.com>
2024-04-24 21:16:29 +0800

    添加redis

--- spzx-service/service-product/src/main/java/com/aqiu/spzx/product/service/impl/CategoryServiceImpl.java
+++ spzx-service/service-product/src/main/java/com/aqiu/spzx/product/service/impl/CategoryServiceImpl.java
@@ -1,12 +1,17 @@
 package com.aqiu.spzx.product.service.impl;
 
+import com.alibaba.fastjson.JSON;
 import com.aqiu.spzx.model.entity.product.Category;
 import com.aqiu.spzx.product.mapper.CategoryMapper;
 import com.aqiu.spzx.product.service.CategoryService;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 @Service
@@ -15,12 +20,22 @@ public class CategoryServiceImpl implements CategoryService {
     @Autowired
     private CategoryMapper categoryMapper;
 
+    @Autowired
+    private RedisTemplate<String,String> redisTemplate;
+
     @Override
     public List<Category> selectOneCategory() {
-        return categoryMapper.selectOneCategory();
+        String categoryOneJson = redisTemplate.opsForValue().get("category:one");
+        if (StringUtils.isNotBlank(categoryOneJson)) {
+            return JSON.parseArray(categoryOneJson, Category.class);
+        }
+        List<Category> categoryList = categoryMapper.selectOneCategory();
+        redisTemplate.opsForValue().set("category:one", JSON.toJSONString(categoryList),7, TimeUnit.DAYS);
+        return categoryList;
     }
 
     @Override
+    @Cacheable(value = "category",key = "'all'")
     public List<Category> findCategoryTree() {
         List<Category> allData = categoryMapper.findAll();
         List<Category> oneCategory = allData.stream().filter(category -> category.getParentId() == 0).collect(Collectors.toList());
